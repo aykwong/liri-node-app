69df299dc1f0d44c5f545a26b7c4247bab7e4958
Alpha <aykwong93@gmail.com>
2018-07-27 08:15:14 -0400

    Fixed keys.js module

--- liri.js
+++ liri.js
@@ -1,10 +1,12 @@
 require("dotenv").config();
 
-var keys = require(`keys`);
-var spotify = new Spotify(keys.spotify);
-var client = new Twitter(keys.twitter);
+var keys = require('./keys');
+var fs = require('fs');
 var command = process.argv[2];
 var content = process.argv[3];
+var spotify = new spotify(keys.spotify);
+var client = new twitter(keys.twitter);
+var omdb = `http://www.omdbapi.com/?t="${content}"&y=&plot=short&apikey=trilogy`;
 
 switch (command) {
   case "my-tweets":
@@ -25,40 +27,53 @@ switch (command) {
 }
 
 function spotify() {
+  if (!content) {
+    content = "The Sign";
+  }
 
   spotify.search({ type: "track", query: content }, function(err, data) {
     if (err) {
       return console.log("Error occurred: " + err);
     }
 
-    console.log(`Artists: ${data.artists}`, `\nTrack: ${content}`, `\nPreview: ${data.preview_url}`, `\nAlbum: ${data.name}`);
+    console.log(
+      `Artists: ${data.artists}`,
+      `\nTrack: ${content}`,
+      `\nPreview: ${data.preview_url}`,
+      `\nAlbum: ${data.name}`
+    );
   });
-
 };
 
-// fs.readFile(textFile, "utf8", function(err, data) {
-//   if (err) {
-//     return console.log(err);
-//   }
-//   data = data.split(",");
-//   var song = data[1];
-// });
+function omdb() {
+  if (!content) {
+    content = "Mr. Nobody";
+  }
 
+  request(omdb, function(error, response, body) {
+    if (!error && response.statusCode === 200) {
+      body = JSON.parse(body);
+      console.log(
+        `Title: ${body.Title}`,
+        `\nYear: ${body.Year}`,
+        `\nIMDB Rating: ${body.imdbRating}`,
+        `\n${body.Ratings[0].Source} Rating: ${body.Ratings[0].Value}`,
+        `\nCountry Produced: ${body.Country}`,
+        `\nLanguage: ${body.Language}`,
+        `\nPlot: ${body.Plot}`,
+        `\nActors: ${body.Actors}`
+      );
+    }
+  });
+}
 
-
-// var queryUrl = {
-//     twitter = "",
-//     spotify = "",
-//     omdb = `http://www.omdbapi.com/?t="${movieName}"&y=&plot=short&apikey=trilogy`,
-// }
-
-// fs.readFile(textFile, "utf8", function(err, data) {});
-
-// request(queryUrl, function(error, response, body) {
-//   // If the request is successful (i.e. if the response status code is 200)
-//   if (!error && response.statusCode === 200) {
-//     // Parse the body of the site and recover just the imdbRating
-//     // (Note: The syntax below for parsing isn't obvious. Just spend a few moments dissecting it).
-//     console.log("The movie's rating is: " + JSON.parse(body).imdbRating);
-//   }
-// });
+function whatItSays() {
+  fs.readFile("random.txt", "utf8", function(err, data) {
+    if (err) {
+      return console.log("Error occurred: " + err);
+    }
+    data = data.split(",");
+    command = data[0];
+    content = data[1];
+  });
+}
